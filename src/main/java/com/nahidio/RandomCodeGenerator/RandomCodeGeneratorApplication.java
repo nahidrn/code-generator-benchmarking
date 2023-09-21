f6469f42bd7ed4278805b37f1d18a7606fc7d6f0
nahidrn <sm.nahid.rn@gmail.com>
2023-09-21 12:52:27 +0600

    Init Project

---
+++ src/main/java/com/nahidio/RandomCodeGenerator/RandomCodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.nahidio.RandomCodeGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomCodeGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nahidio/RandomCodeGenerator/RandomCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahidio.RandomCodeGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
